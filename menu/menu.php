49f46d07bdbcf2faff7aa9dc3dee3aaed6e41c9e
Ricardo Tavares <ricardo.tavares@silencor.pt>
2021-11-19 11:14:45 +0000

    feat: create general permission for departement IT change  permissions

--- menu/menu.php
+++ menu/menu.php
@@ -1,17 +1,26 @@
 ﻿<?php
-
 	require_once $_SERVER['DOCUMENT_ROOT'].'/include/base.php';
 	$db = new mysqli($MYSQL_HOST, $MYSQL_USER, $MYSQL_PWD, $MYSQL_DBINTRANET) or die("Connect failed: %s\n". mysqli_connect_error());
 
+	$sql_00 = "	SELECT 
+					* 
+				FROM 
+					users 
+				WHERE 
+						departamento like '%nform%ticos%'
+					AND login = '".$res_login."'  ";
+	$result_00 = $db->query($sql_00) or die($sql_00 . '-' . $db->error  );
 
+	$PERMISSIONS = ( $result_00->num_rows > 0 ? 1 : 0 );
+ 
 	header("Content-type: text/xml");
 	echo('<?xml version="1.0" encoding="UTF-8"?>');
 	echo('<menu>');		
-		/*
-		echo('<item id="dashboard" text="" img="home.png">');
-			echo('<userdata name="permissoes">'.json_encode($permissoes).'</userdata>');
-		echo('</item>');	
-	*/
+		if( $PERMISSIONS ){
+			echo('<item text="Permissions" img="key.png">');
+				echo('<item id="permissoes_banner_qualidade" text="Banner Qualidade" img="key.png" />');
+			echo('</item>');	
+		}
 	echo('</menu> ');			
 	$db->close();
 ?>
